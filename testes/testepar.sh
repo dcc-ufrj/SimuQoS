db9261e86934dfa8765fbd0d5a58aff6b3f0f2e3
diogo <diogo.mvieira@gmail.com>
2014-01-23 14:00:58 -0200

    final commit

---
+++ testes/testepar.sh
@@ -0,0 +1,31 @@
+#rm -rf ../resultado/aleatory/*
+#rm -rf ../resultado/sequential/*
+for i in 1 2 3 4 5
+do
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 sequential 10 120 aleatory 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 aleatory 10 120 aleatory 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 sequential 10 120 sequential 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 aleatory 10 120 sequential 30 5;
+
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 sequential 30 120 aleatory 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 aleatory 30 120 aleatory 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 sequential 30 120 sequential 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 aleatory 30 120 sequential 30 5;
+
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 sequential 50 120 aleatory 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 aleatory 50 120 aleatory 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 sequential 50 120 sequential 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 aleatory 50 120 sequential 30 5;
+
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 sequential 70 120 aleatory 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 aleatory 70 120 aleatory 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 sequential 70 120 sequential 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 aleatory 70 120 sequential 30 5;
+
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 sequential 100 120 aleatory 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 aleatory 100 120 aleatory 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 sequential 100 120 sequential 30 5;
+python ../classes/Main.py 50 120 120 5000 unitario aleatorio 1 aleatory 100 120 sequential 30 5;
+echo $i;
+done
+#python ../classes/Main.py
